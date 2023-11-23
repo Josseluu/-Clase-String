3ef5169661ef5f7ecc299151f3e3c721a4f8200a
Josseluu <jslslpz@myuax.com>
2023-11-23 12:12:55 +0100

    Initial commit

--- Funciones de base de la clase string.cpp
+++ Funciones de base de la clase string.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 #include <string>
+
+int main() {
+
+    std::string miCadena = "Hola, mundo!";
+
+    std::cout << "El tamaño de la cadena es: " << miCadena.size() << std::endl;
+
+
+}
