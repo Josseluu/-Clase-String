a4a2d51868411ad8fbd2c9e38de1863f4e906040
Josseluu <jslslpz@myuax.com>
2023-11-23 12:21:46 +0100

    Initial commit

--- Manipulación avanzada de strings.cpp
+++ Manipulación avanzada de strings.cpp
@@ -3,3 +3,4 @@
 int main() {
     std::string str = "Inteligencia Artificial";
     std::string sub_str = str.substr(13, 9);
+    std::string new_str = str.insert(0, "La "); // "La Inteligencia Artificial"
