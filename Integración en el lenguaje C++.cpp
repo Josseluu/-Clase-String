1b89e8a6120b00c8038f104d3977ae122e2f40b3
Josseluu <jslslpz@myuax.com>
2023-11-23 12:15:50 +0100

    Initial commit

--- Integración en el lenguaje C++.cpp
+++ Integración en el lenguaje C++.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 #include <string>
 
+int main() {
+    std::string str1 = "Hola ";
+    std::string str2 = "mundo!";
+    std::string str3 = str1 + str2;
+    std::cout << "Cadena resultante: " << str3 << std::endl;
+    return 0;
+}
