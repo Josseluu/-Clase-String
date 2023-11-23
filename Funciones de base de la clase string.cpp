fd2c18d06b50545891900277f8ddbcd15eed532c
Josseluu <jslslpz@myuax.com>
2023-11-23 12:13:37 +0100

    Initial commit

--- Funciones de base de la clase string.cpp
+++ Funciones de base de la clase string.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
 #include <string>
 
+#include <iostream>
+#include <string>
+
 int main() {
 
     std::string miCadena = "Hola, mundo!";
 
     std::cout << "El tamaño de la cadena es: " << miCadena.size() << std::endl;
 
-
-    int main() {
-
-        std::string miCadena = "Hola, mundo!";
-
-        std::cout << "El tamaño de la cadena es: " << miCadena.size() << std::endl;
-
-        if (miCadena.empty()) {
-            std::cout << "La cadena está vacía." << std::endl;
-        } else {
-            std::cout << "La cadena no está vacía." << std::endl;
-        }
-
-        return 0;
+    if (miCadena.empty()) {
+        std::cout << "La cadena está vacía." << std::endl;
+    } else {
+        std::cout << "La cadena no está vacía." << std::endl;
     }
 
+    return 0;
 }
